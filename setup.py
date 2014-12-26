b61db0d456c15a71628c7fbd3a9eda6f59be62f5
Ricardo Champa <Ricardo@MacBook-Pro-de-Ricardo.local>
2014-12-26 01:03:01 +0100

    1. updating requirements 2. updating medatada

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup
 
 setup(name='FlaskApp',
       version='1.0',
-      description='A basic Flask app with static files',
-      author='Ryan Jarvinen',
-      author_email='ryanj@redhat.com',
+      description='Scheduler',
+      author='Ricardo Champa',
+      author_email='sonrie.clickmobile@gmail.com',
       url='http://www.python.org/sigs/distutils-sig/',
      install_requires=['Flask>=0.10.1'],
      )
