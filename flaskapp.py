05cb962656e54f9620a8af167aeaa36924661bf5
Ricardo Champa <Ricardo@MacBook-Pro-de-Ricardo.local>
2014-12-26 19:37:27 +0100

    1. auth 2. ws 3. registration with sqlalchemy

--- flaskapp.py
+++ flaskapp.py
@@ -1,10 +1,14 @@
 import os
 from datetime import datetime
 from flask import Flask, request, flash, url_for, redirect, \
-     render_template, abort, send_from_directory
+     render_template, abort, send_from_directory, session
 
 app = Flask(__name__)
 app.config.from_pyfile('flaskapp.cfg')
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/ric_cm_notificaciones'
+from models import db
+db.init_app(app)
 
 @app.route('/')
 def index():
@@ -17,6 +21,199 @@ def serveStaticResource(resource):
 @app.route("/test")
 def test():
     return "<strong>It's Alive!</strong>"
+
+@app.route('/testdb')
+def testdb():
+  if db.session.query("1").from_statement("SELECT 1").all():
+    return 'It works.'
+  else:
+    return 'Something is broken.'
+
+
+#Importing forms, preventing a CSRF attack
+from forms import SignupForm, SigninForm
+from models import db, Administrador
+
+@app.route('/home')
+def home():
+ 
+  if 'email' not in session:
+    return redirect(url_for('signin'))
+ 
+  user = Administrador.query.filter_by(email = session['email']).first()
+ 
+  if user is None:
+    return redirect(url_for('signin'))
+  else:
+    return render_template('home.html')
+
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+  form = SignupForm()
+   
+  if request.method == 'POST':
+    if form.validate() == False:
+      return render_template('signup.html', form=form)
+    else:   
+      newuser = Administrador(form.email.data, form.password.data)
+      db.session.add(newuser)
+      db.session.commit()
+
+      #Saving session variable 
+      session['email'] = newuser.email
+
+      #return "[1] Create a new user [2] sign in the user [3] redirect to the user's home"
+      return redirect(url_for('home'))
+   
+  elif request.method == 'GET':
+    return render_template('signup.html', form=form)
+
+
+@app.route('/signin', methods=['GET', 'POST'])
+def signin():
+  form = SigninForm()
+   
+  if request.method == 'POST':
+    if form.validate() == False:
+      return render_template('signin.html', form=form)
+    else:
+      session['email'] = form.email.data
+      return redirect(url_for('home'))
+                 
+  elif request.method == 'GET':
+    return render_template('signin.html', form=form)
+
+
+@app.route('/signout')
+def signout():
+ 
+  if 'email' not in session:
+    return redirect(url_for('signin'))
+     
+  session.pop('email', None)
+  return redirect(url_for('index'))
+
+
+
+
+"""Alternative version of the ToDo RESTful server implemented using the Flask-RESTful extension."""
+
+from flask.views import MethodView
+from flask.ext.restful import Api, Resource, reqparse, fields, marshal
+from flask.ext.httpauth import HTTPBasicAuth
+import json,ast
+
+api = Api(app)
+auth = HTTPBasicAuth()
+
+@auth.get_password
+def get_password(username):
+    if username == 'todo':
+        return 'ws'
+    return None
+ 
+@auth.error_handler
+def unauthorized():
+    return make_response(jsonify( { 'message': 'Unauthorized access' } ), 403)
+    # return 403 instead of 401 to prevent browsers from displaying the default auth dialog
+
+
+#v = {'id': 1,'title': u'Buy groceries','description': u'Milk, Cheese, Pizza, Fruit, Tylenol','done': False}
+#app.logger.debug(type(v))
+
+    
+tasks = [
+    {
+        'id': 1,
+        'title': u'Buy groceries',
+        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol', 
+        'done': False
+    },
+    {
+        'id': 2,
+        'title': u'Learn Python',
+        'description': u'Need to find a good Python tutorial on the web', 
+        'done': False
+    }
+]
+ 
+task_fields = {
+    'title': fields.String,
+    'description': fields.String,
+    'done': fields.Boolean,
+    'uri': fields.Url('task')
+}
+
+class TaskListAPI(Resource):
+    decorators = [auth.login_required]
+
+    def __init__(self):
+        self.reqparse = reqparse.RequestParser()
+        self.reqparse.add_argument('title', type = str, required = True, help = 'No task title provided', location = 'json')
+        self.reqparse.add_argument('description', type = str, default = "", location = 'json')
+        super(TaskListAPI, self).__init__()
+        
+    def get(self):
+        return { 'tasks': map(lambda t: marshal(t, task_fields), tasks) }
+
+    def post(self):
+        args = self.reqparse.parse_args()
+
+        if len(tasks)==0:
+            index = 1
+        else:
+            index = tasks[-1]['id'] + 1 # -1 is thr last index
+
+        task = {
+            'id': index, 
+            'title': args['title'],
+            'description': args['description'],
+            'done': False
+        }
+        tasks.append(task)
+        return { 'task': marshal(task, task_fields) }, 201
+
+class TaskAPI(Resource):
+    decorators = [auth.login_required]
+    
+    def __init__(self):
+        self.reqparse = reqparse.RequestParser()
+        self.reqparse.add_argument('title', type = str, location = 'json')
+        self.reqparse.add_argument('description', type = str, location = 'json')
+        self.reqparse.add_argument('done', type = bool, location = 'json')
+        super(TaskAPI, self).__init__()
+
+    def get(self, id):
+        task = filter(lambda t: t['id'] == id, tasks)
+        if len(task) == 0:
+            abort(404)
+        return { 'task': marshal(task[0], task_fields) }
+        
+    def put(self, id):
+        task = filter(lambda t: t['id'] == id, tasks)
+        if len(task) == 0:
+            abort(404)
+        task = task[0]
+        args = self.reqparse.parse_args()
+        for k, v in args.iteritems():
+            if v != None:
+                task[k] = v
+
+        return { 'task': marshal(task, task_fields) }
+
+    def delete(self, id):
+        task = filter(lambda t: t['id'] == id, tasks)
+        if len(task) == 0:
+            abort(404)
+        tasks.remove(task[0])
+        return { 'result': True }
+
+api.add_resource(TaskListAPI, '/todo/api/v1.0/tasks', endpoint = 'tasks')
+api.add_resource(TaskAPI, '/todo/api/v1.0/tasks/<int:id>', endpoint = 'task')
+
+
+
 
 if __name__ == '__main__':
     app.run()
